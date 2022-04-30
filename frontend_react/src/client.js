3ad3cfff5674f1655819fd13749f8a0cef7b9f7c
mariana-k <mariana.kyrkosh@gmail.com>
2022-04-30 19:59:26 +0300

    Env variables

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: "ofcxhojc",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: "sk3KxDRvLjSUYcUT2lZwg0nKu2vKUv9bsh0At7KLCltWSv8v4AN9zyFop6G1Mn7o6sMwwGLfIEHtnsxaxglKfKG09PNQcGWelurqmexSo3gpeX3CVxxe2Be12wjc6v0wzqlxvEwpSA2BPAKcTgptsgygKHw9RbaE0WgW7uhHbESa1bo1eEcp",
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 console.log(client)
 
